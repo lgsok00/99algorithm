6977bc75e36dcf7e8e005884d178ca167e4820d9
lgsok00 <lgsok00@naver.com>
2024-05-30 15:30:49 +0900

    1152 - 단어의 갯수

---
+++ src/main/java/day02/Q_1152.java
@@ -0,0 +1,20 @@
+package day02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+  1. BufferedReader 로 입력 받기
+  2. StringTokenizer 로 공백(" ")을 기준으로 문자열 분리하기
+ */
+public class Q_1152 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    System.out.println(st.countTokens());
+  }
+}
