2d663055340ed6eaf490402c6f44587b8acd9ab4
lgsok00 <lgsok00@naver.com>
2024-06-03 15:26:10 +0900

    2738 - 행렬 덧셈

---
+++ src/main/java/day05/Q_2738.java
@@ -0,0 +1,38 @@
+package day05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_2738 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[][] arr = new int[n][m];
+
+    // 첫번째 행렬에 값 추가
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    // 두 번째 행렬에 값을 넣어주면서 StringBuilder 애 값 저장
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++) {
+        sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+}
